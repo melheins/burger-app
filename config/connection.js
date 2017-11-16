5228a96e2570e2627f173f19b6dd4950c35fe121
melheins <heins.mel@gmail.com>
2017-11-16 18:13:38 -0500

    fix

--- config/connection.js
+++ config/connection.js
@@ -5,19 +5,20 @@ var dotenv = require('dotenv').config();
 var connection;
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env_JAWSDB_URL)
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
 }
 else {
-connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_DATABASE
-});}
+    connection = mysql.createConnection({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_DATABASE
+    });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error("error connecting: " + err.stack);
         return;
